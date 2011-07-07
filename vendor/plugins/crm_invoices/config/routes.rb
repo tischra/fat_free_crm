23ac7346a596d483d3388c2f28ca65cdcb13578b
root <root@huynhut-Inspiron-N5010.(none)>
2011-07-07 21:59:13 +0700

    change

--- vendor/plugins/crm_invoices/config/routes.rb
+++
@@ -1,5 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  
-   map.resources :invoices,      :has_many => :comments, :collection => { :search => :get, :auto_complete => :post, :options => :get, :laterun => :get, :redraw => :post, :redraw_late => :post }
-
-end
